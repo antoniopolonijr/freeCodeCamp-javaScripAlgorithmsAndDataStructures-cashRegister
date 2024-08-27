b418131ed01f69da3bc88b31a83c2bd760100a4f
Antonio Poloni <antoniopolonijr@gmail.com>
2024-08-27 16:35:33 -0300

    Implement checkCash function and update html

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 /// Cash Register
 
+// Initial variables
 let price = 1.87;
 let cid = [
   ["PENNY", 1.01],
@@ -13,6 +14,19 @@ let cid = [
   ["ONE HUNDRED", 100],
 ];
 
+// Currency units and their values
+const currencyUnits = [
+  ["ONE HUNDRED", 100.0],
+  ["TWENTY", 20.0],
+  ["TEN", 10.0],
+  ["FIVE", 5.0],
+  ["ONE", 1.0],
+  ["QUARTER", 0.25],
+  ["DIME", 0.1],
+  ["NICKEL", 0.05],
+  ["PENNY", 0.01],
+];
+
 // variables assigned to access elements in HTML document
 const totalPrice = document.getElementById("total-price");
 const cash = document.getElementById("cash"); // set the "cash"
@@ -22,14 +36,48 @@ const changeDue = document.getElementById("change-due"); // set the "output"
 
 // Functions
 
-// update html text
-totalPrice.textContent = `Total Price: $${price}`;
+const checkCash = (cash) => {
+  // Check if the customer has enough money
+  if (cash < price) {
+    alert("Customer does not have enough money to purchase the item");
+    return;
+  }
+
+  // Handle exact cash payment
+  if (cash === price) {
+    changeDue.innerHTML =
+      "<p>No change due - customer paid with exact cash</p>";
+    return;
+  }
+
+  calculateChange(cash);
+};
+
+// Calculate the change due
+const calculateChange = (cash) => {
+  let currentChange = (cash - price).toFixed(2);
+  const totalCID = cid
+    .reduce((total, currency) => total + currency[1], 0)
+    .toFixed(2);
+
+  if (currentChange > totalCID) {
+    changeDue.innerHTML = "<p>Status: INSUFFICIENT_FUNDS</p>";
+    return;
+  }
+};
+
+// update html
+totalPrice.textContent = `Total Price: $${price.toFixed(2)}`;
+
+cid.forEach((element) => {
+  changeInDrawer.innerHTML += `<p>${element[0]}: $${element[1].toFixed(2)}</p>`;
+});
 
 // Event Listeners
 
 // event listener to call the checkCash function when users click the Check button.
 purchaseBtn.addEventListener("click", () => {
-  checkCash(cash.value); // take the value of text input and execute the checkCash function
+  checkCash(Number(cash.value)); // take the value of text input and execute the checkCash function
   cash.value = ""; // clear the input text in the html
 });
 
@@ -37,7 +85,7 @@ purchaseBtn.addEventListener("click", () => {
 cash.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     e.preventDefault(); // method to stop the browser from refreshing the page after submitting the form.
-    checkCash(cash.value); // take the value of text input and execute the checkCash function
+    checkCash(Number(cash.value)); // take the value of text input and execute the checkCash function
     cash.value = ""; // clear the input text in the html
   }
 });
